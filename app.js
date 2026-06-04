a280668f3a5a1626876127c3fb93c860c7a84bb5
Warley <mwarley51@gmail.com>
2026-06-03 23:21:02 -0300

    adicionando mais uma condição de valores vazio

--- app.js
+++ app.js
@@ -3,18 +3,22 @@ const bntBotao = document.getElementById('btn-sortear');
 function gerarNumero() {
     const min = Math.ceil(document.getElementById('input-min').value);
     const max = Math.floor(document.getElementById('input-max').value);
-    const mensagem = document.querySelector('h2');
     const numeroFinal = document.querySelector('.numero-final');
+    const mensagem = document.querySelector('.mensagem');
+    const valorFinal = document.querySelector('.valor-final');
 
-    if (min >= max) {
-        mensagem.innerHTML = 'O valor minimo não pode ser maior ou igual o valor maximo : (';
+    if (min == '' || max == '') {
+        alert('Digite um valor')
+    } else if (min >= max) {
+        mensagem.innerHTML = 'O numero minino não pode ser maior que o numero maximo 😞';
         mensagem.style.color = 'red';
-        numeroFinal.innerHTML = ''; numeroFinal.style.cssText = 'border: none; backgroundColor: #111828;';
+        numeroFinal.style.display = 'none';
+
     } else {
         const resultado = Math.floor(Math.random() * (max - min + 1)) + min;
-        numeroFinal.innerHTML = resultado; numeroFinal.style.border = '2px solid #00FF7F';
-        mensagem.innerHTML = 'O numero sorteado é:';
-        mensagem.style.color = '';
+        valorFinal.innerHTML = resultado;
+        mensagem.innerHTML = `<h2 class="mensagem">O numero sorteado é:</h2>`;
+        numeroFinal.style.display = 'block';
     }
 }
 
